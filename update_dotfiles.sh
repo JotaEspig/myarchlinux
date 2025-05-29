5a13c4c727870f7f42c0bf160844e50114a84372
João Vitor Espig <jotaespig@gmail.com>
2025-05-29 15:29:16 -0300

    update

---
+++ update_dotfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -r ~/.config/kitty ./dotfiles/kitty
+cp ~/.config/betterlockscreenrc ./dotfiles/betterlockscreenrc
+cp ~/.config/picom.conf ./dotfiles/picom.conf
+cp -r ~/.config/i3 ./dotfiles/i3
+cp -r ~/.config/i3status ./dotfiles/i3status
+cp -r ~/.config/polybar ./dotfiles/polybar
